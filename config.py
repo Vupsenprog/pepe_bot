53a71e19c065653be7f0f4595160059c88b01f95
Vupsenprog <vupsenbit@gmail.com>
2026-02-22 22:18:04 +0300

    config.py

--- config.py
+++ config.py
@@ -2,10 +2,7 @@
 # Этот файл содержит все настройки, которые вы можете менять
 
 # ---------- Токены (обязательно) ----------
-VK_TOKEN = "vk1.a.m7dsvCno8W9WxhbycYrPq49Iwp124wqRClq42xby-Pqul04IEFoHZccqFCzg8oFuByp0VXwG1lqsm0SEyb1584uSh5h-mBxf8rRLcrT6LZK6WwhT2NW2Z01JD2PYT8XBnad1UB67LjUqjtCnE10-jMHEBIfKCOMXyrkv_VvsaCEAUIOZFIyiLttMDNKZFKC0932DMzkqtVBt8dzPrHmjRw"
-TG_TOKEN = "8272252531:AAEMFGA_c1bnCBnJb-TQ3n_mKFhRl13fR2k"
-TG_CHAT_ID = "1295617189"
-GROUP_DOMAIN = "club236197695"
+VK_TOKEN = 
 
 # ---------- Ключевые слова для поиска ----------
 # Можно добавлять или убирать слова через запятую
